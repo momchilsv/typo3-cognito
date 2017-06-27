db195866ef1afbd458a9aae7ab7a3d54c937e2cb
Momchil Vangelov <mva@peytz.dk>
2017-06-27 12:19:59 +0300

    [TASK] Composer mode compatiblity

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,27 +11,27 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Cognito authentication',
-	'description' => 'Cognito authentication service for TYPO3 CMS',
-	'category' => 'services',
-	'version' => '0.0.2',
-	'dependencies' => 'typo3',
-	'state' => 'beta',
-	'uploadfolder' => 0,
-	'clearCacheOnLoad' => 0,
-	'author' => 'Momchil Vangelov',
-	'author_email' => 'mva@peytz.dk',
-	'author_company' => 'Peytz & Co',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.6.0-7.6.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => 'a:0:{}',
-	'suggests' => array(
-	),
+    'title' => 'Cognito authentication',
+    'description' => 'Cognito authentication service for TYPO3 CMS',
+    'category' => 'services',
+    'version' => '0.0.3',
+    'dependencies' => 'typo3',
+    'state' => 'beta',
+    'uploadfolder' => 0,
+    'clearCacheOnLoad' => 0,
+    'author' => 'Momchil Vangelov',
+    'author_email' => 'mva@peytz.dk',
+    'author_company' => 'Peytz & Co',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-7.6.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    '_md5_values_when_last_written' => 'a:0:{}',
+    'suggests' => array(
+    ),
 );
--- ext_tables.sql
+++ ext_tables.sql
@@ -2,5 +2,5 @@
 # Table structure for table 'fe_users'
 #
 CREATE TABLE fe_users (
-	tx_cognito_cognito_id varchar(255) DEFAULT '' NOT NULL
+  tx_cognito_cognito_id varchar(255) DEFAULT '' NOT NULL
 );
